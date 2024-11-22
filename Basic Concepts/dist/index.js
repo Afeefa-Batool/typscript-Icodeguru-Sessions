2cb7b2824a7cebdb48005029413b933934f40a75
Afeefa Batool <afeefabatoolpgc@gmail.com>
2024-11-22 11:52:45 +0500

    Initial commit

---
+++ Basic Concepts/dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+let person = ['Alice', 30];
+console.log(person[0]);
+console.log(person[1]);
+//# sourceMappingURL=index.js.map
---
+++ Basic Concepts/src/index.ts
@@ -0,0 +1,8 @@
+// let a:[string,number]=[1,"adfgg"];
+
+
+// console.log(a[0]);
+let person: [string, number] = ['Alice', 30];
+
+console.log(person[0]); 
+console.log(person[1]); 
